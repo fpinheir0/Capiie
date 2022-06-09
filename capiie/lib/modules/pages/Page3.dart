bbcc6a7d9b9d8b89248bdcb0503ecdde4a7965d1
fepinheir0 <f.pinheir0@outlook.com>
2022-06-09 11:43:21 -0300

    adicionando campos necessários para criar o cartão virtual

--- capiie/lib/modules/pages/Page3.dart
+++ capiie/lib/modules/pages/Page3.dart
@@ -1,4 +1,6 @@
 import 'package:avatar_glow/avatar_glow.dart';
+import 'package:capiie/modules/pages/Page4.dart';
+import 'package:capiie/utilidades/delayed_animation.dart';
 import 'package:flutter/material.dart';
 
 class page3 extends StatefulWidget {
@@ -16,41 +18,152 @@ class _page3State extends State<page3> {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(12.0),
-          child: Column(children: <Widget>[
-            Container(
-              child: Center(
-                child: AvatarGlow(
-                  endRadius: 70,
-                  duration: Duration(seconds: 3),
-                  glowColor: Color(0xFF9195FF),
-                  repeat: true,
-                  repeatPauseDuration: Duration(seconds: 2),
-                  startDelay: Duration(seconds: 1),
-                  child: Material(
-                      elevation: 8.0,
-                      shape: CircleBorder(),
-                      child: CircleAvatar(
-                        backgroundColor: Colors.grey[100],
-                        child: Image.asset(
-                          'assets/images/capiie.png',
-                          width: 45,
-                        ),
-                        radius: 30.0,
-                      )),
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: Center(
+                  child: AvatarGlow(
+                    endRadius: 70,
+                    duration: Duration(seconds: 3),
+                    glowColor: Color(0xFF9195FF),
+                    repeat: true,
+                    repeatPauseDuration: Duration(seconds: 2),
+                    startDelay: Duration(seconds: 1),
+                    child: Material(
+                        elevation: 8.0,
+                        shape: CircleBorder(),
+                        child: CircleAvatar(
+                          backgroundColor: Colors.grey[100],
+                          child: Image.asset(
+                            'assets/images/capiie.png',
+                            width: 45,
+                          ),
+                          radius: 30.0,
+                        )),
+                  ),
                 ),
               ),
-            ),
-            Text(
-              "Shooow! seja bem vindo! vamos começar a criação do seu cartão de vez!",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20.0,
-                  color: Colors.white),
-            )
-          ]),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "Shooow! seja bem vindo! vamos começar a criação do seu cartão de vez!",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
+                      color: Colors.white),
+                ),
+              ),
+              SizedBox(
+                height: 1.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "Preencha as demais informações",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
+                      color: Colors.white),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: DelayedAnimation(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'CARGO',
+                      ),
+                    ),
+                    delay: delayedAmount + 1000,
+                    direction: 'up'),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: DelayedAnimation(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'EMAIL',
+                      ),
+                    ),
+                    delay: delayedAmount + 1000,
+                    direction: 'up'),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: DelayedAnimation(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'WHATSAPP',
+                      ),
+                    ),
+                    delay: delayedAmount + 1000,
+                    direction: 'up'),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: DelayedAnimation(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'TELEFONE',
+                      ),
+                    ),
+                    delay: delayedAmount + 1000,
+                    direction: 'up'),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              DelayedAnimation(
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => page4()));
+                    },
+                    child: _animatedButtonUI,
+                  ),
+                  delay: delayedAmount + 7000,
+                  direction: 'up'),
+            ],
+          ),
         ),
       ),
     );
   }
+
+  Widget get _animatedButtonUI => Container(
+        height: 60,
+        width: 270,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100.0),
+          color: Colors.white,
+        ),
+        child: Center(
+          child: Text(
+            'CONTINUE',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              color: Color(0xFF8185E2),
+            ),
+          ),
+        ),
+      );
 }
