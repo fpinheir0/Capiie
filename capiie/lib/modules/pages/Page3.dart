0bb2bf6d1b99c9d1878dab631b51f70fcdeeb69e
fepinheir0 <f.pinheir0@outlook.com>
2022-06-09 11:22:26 -0300

    criando pagina 3

---
+++ capiie/lib/modules/pages/Page3.dart
@@ -0,0 +1,56 @@
+import 'package:avatar_glow/avatar_glow.dart';
+import 'package:flutter/material.dart';
+
+class page3 extends StatefulWidget {
+  @override
+  _page3State createState() => _page3State();
+}
+
+class _page3State extends State<page3> {
+  final int delayedAmount = 500;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF8185E2),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(children: <Widget>[
+            Container(
+              child: Center(
+                child: AvatarGlow(
+                  endRadius: 70,
+                  duration: Duration(seconds: 3),
+                  glowColor: Color(0xFF9195FF),
+                  repeat: true,
+                  repeatPauseDuration: Duration(seconds: 2),
+                  startDelay: Duration(seconds: 1),
+                  child: Material(
+                      elevation: 8.0,
+                      shape: CircleBorder(),
+                      child: CircleAvatar(
+                        backgroundColor: Colors.grey[100],
+                        child: Image.asset(
+                          'assets/images/capiie.png',
+                          width: 45,
+                        ),
+                        radius: 30.0,
+                      )),
+                ),
+              ),
+            ),
+            Text(
+              "Shooow! seja bem vindo! vamos começar a criação do seu cartão de vez!",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0,
+                  color: Colors.white),
+            )
+          ]),
+        ),
+      ),
+    );
+  }
+}
