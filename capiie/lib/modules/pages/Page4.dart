0ba42ddb78dca4515efef3daebf4827a4fa01073
fepinheir0 <f.pinheir0@outlook.com>
2022-06-13 18:30:42 -0300

    refatorando codigos da pagina 3 na pagina 4 e 5

--- capiie/lib/modules/pages/Page4.dart
+++ capiie/lib/modules/pages/Page4.dart
@@ -1,5 +1,5 @@
 import 'package:avatar_glow/avatar_glow.dart';
-import 'package:capiie/modules/pages/page_camera.dart';
+import 'package:capiie/modules/pages/page5.dart';
 import 'package:capiie/utilidades/delayed_animation.dart';
 import 'package:flutter/material.dart';
 
@@ -11,6 +11,18 @@ class page4 extends StatefulWidget {
 class _page4State extends State<page4> {
   final int delayedAmount = 500;
 
+  TextEditingController txtEmail = new TextEditingController();
+
+  void Salvar() {
+    String email;
+
+    setState(() {
+      email = txtEmail.text;
+
+      Navigator.push(context, MaterialPageRoute(builder: (context) => page5()));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +30,7 @@ class _page4State extends State<page4> {
       backgroundColor: Color(0xFF8185E2),
       body: Center(
         child: Padding(
-          padding: EdgeInsets.all(12.0),
+          padding: const EdgeInsets.all(12.0),
           child: Column(
             children: <Widget>[
               Container(
@@ -31,34 +43,53 @@ class _page4State extends State<page4> {
                     repeatPauseDuration: Duration(seconds: 2),
                     startDelay: Duration(seconds: 1),
                     child: Material(
-                      elevation: 8.0,
-                      shape: CircleBorder(),
-                      child: CircleAvatar(
-                        backgroundColor: Colors.grey[100],
-                        child: Image.asset(
-                          'assets/images/capiie.png',
-                          width: 45,
-                        ),
-                        radius: 30.0,
-                      ),
-                    ),
+                        elevation: 8.0,
+                        shape: CircleBorder(),
+                        child: CircleAvatar(
+                          backgroundColor: Colors.grey[100],
+                          child: Image.asset(
+                            'assets/images/capiie.png',
+                            width: 45,
+                          ),
+                          radius: 30.0,
+                        )),
                   ),
                 ),
               ),
               Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text(
-                  "Que tal tirarmos uma foto para inserirmos em seu cartão ?",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20.0,
-                      color: Colors.white),
-                ),
+                padding: const EdgeInsets.only(right: 20.0, left: 20),
+                child: DelayedAnimation(
+                    child: Text(
+                      "Agora me diga seu e-mail ?",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
+                          color: Colors.white),
+                    ),
+                    delay: delayedAmount + 2000,
+                    direction: 'up'),
               ),
               SizedBox(
-                height: 100,
+                height: 100.0,
               ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: DelayedAnimation(
+                    child: TextFormField(
+                      controller: txtEmail,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Email',
+                      ),
+                    ),
+                    delay: delayedAmount + 5000,
+                    direction: 'up'),
+              ),
+              SizedBox(
+                height: 40.0,
+              ),
+              Divider(),
               Expanded(
                 child: Align(
                   alignment: Alignment.bottomCenter,
@@ -67,14 +98,11 @@ class _page4State extends State<page4> {
               DelayedAnimation(
                   child: GestureDetector(
                     onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => pageCamera()));
+                      Salvar();
                     },
                     child: _animatedButtonUI,
                   ),
-                  delay: delayedAmount + 1000,
+                  delay: delayedAmount + 7000,
                   direction: 'up'),
             ],
           ),
--- capiie/lib/modules/pages/page5.dart
+++ capiie/lib/modules/pages/page5.dart
@@ -1,9 +1,134 @@
-//REFATORAR CAMPOS PARA QUE FIQUE CADA CAMPO EM UMA PAGE
-//COLOCAR VERIFICAÇÃO PARA QUE NÃO PASSE CAMPOS VAZIOS.
-//CASO FIQUE MUITO VAZIA A TELA COLOCAR IMG https://www.manypixels.co/gallery
-//CRIAR UMA PAGINA NO FINAL COM UM RESUMÃO E USAR ELE PARA ENVIAR OS DADOS PARA O BANCO
-//PAGE_CAMERA TERÁ QUE SER TELA FULL SCREEN
-//ALGUM LOADING NO FINAL
-//TENTAR IMPLEMENTAR UM JSON PARA QUE GERE MENSAGENS ALEATORIAS
-//API PARA REALIZAR A INTEGRAÇÃO COM O BANCO DE DADOS
-//POST DA ULTIMA TELA PARA O BANCO E O BANCO DEVOLVE UMA URL PARA GERAR O QRCODE
+import 'package:avatar_glow/avatar_glow.dart';
+import 'package:capiie/modules/pages/page_foto.dart';
+import 'package:capiie/modules/pages/page_resumo.dart';
+import 'package:capiie/utilidades/delayed_animation.dart';
+import 'package:flutter/material.dart';
+
+class page5 extends StatefulWidget {
+  @override
+  _page5State createState() => _page5State();
+}
+
+class _page5State extends State<page5> {
+  final int delayedAmount = 500;
+
+  TextEditingController txtTelefone = new TextEditingController();
+
+  void Salvar() {
+    String telefone;
+
+    setState(() {
+      telefone = txtTelefone.text;
+
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => pageFoto()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Color(0xFF8185E2),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: Center(
+                  child: AvatarGlow(
+                    endRadius: 70,
+                    duration: Duration(seconds: 3),
+                    glowColor: Color(0xFF9195FF),
+                    repeat: true,
+                    repeatPauseDuration: Duration(seconds: 2),
+                    startDelay: Duration(seconds: 1),
+                    child: Material(
+                        elevation: 8.0,
+                        shape: CircleBorder(),
+                        child: CircleAvatar(
+                          backgroundColor: Colors.grey[100],
+                          child: Image.asset(
+                            'assets/images/capiie.png',
+                            width: 45,
+                          ),
+                          radius: 30.0,
+                        )),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 20.0, left: 20),
+                child: DelayedAnimation(
+                    child: Text(
+                      "Quem sabe possamos conversar, me passa seu telefone/WhatsApp?",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
+                          color: Colors.white),
+                    ),
+                    delay: delayedAmount + 2000,
+                    direction: 'up'),
+              ),
+              SizedBox(
+                height: 100.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: DelayedAnimation(
+                    child: TextFormField(
+                      controller: txtTelefone,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Telefone/WhatsApp',
+                      ),
+                    ),
+                    delay: delayedAmount + 5000,
+                    direction: 'up'),
+              ),
+              SizedBox(
+                height: 40.0,
+              ),
+              Divider(),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                ),
+              ),
+              DelayedAnimation(
+                  child: GestureDetector(
+                    onTap: () {
+                      Salvar();
+                    },
+                    child: _animatedButtonUI,
+                  ),
+                  delay: delayedAmount + 7000,
+                  direction: 'up'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget get _animatedButtonUI => Container(
+        height: 60,
+        width: 270,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100.0),
+          color: Colors.white,
+        ),
+        child: Center(
+          child: Text(
+            'CONTINUE',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              color: Color(0xFF8185E2),
+            ),
+          ),
+        ),
+      );
+}
