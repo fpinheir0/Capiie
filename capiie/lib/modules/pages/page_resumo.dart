91d6d4cb28329a58d4ab303253dba2ab52eaa913
fepinheir0 <f.pinheir0@outlook.com>
2022-06-13 18:30:20 -0300

    page resumo de todas as informações para enviar para o banco

---
+++ capiie/lib/modules/pages/page_resumo.dart
@@ -0,0 +1,56 @@
+import 'package:avatar_glow/avatar_glow.dart';
+import 'package:capiie/modules/pages/Page4.dart';
+import 'package:flutter/material.dart';
+
+class PageResumo extends StatefulWidget {
+  @override
+  _PageResumoState createState() => _PageResumoState();
+}
+
+class _PageResumoState extends State<PageResumo>
+    with SingleTickerProviderStateMixin {
+  final int delayedAmount = 500;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //   _scale = 1 - _controller.value;
+    return Scaffold(
+      backgroundColor: Color(0xFF8185E2),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(12.0),
+          child: Column(children: <Widget>[
+            Container(
+              child: Center(
+                child: AvatarGlow(
+                  endRadius: 70,
+                  duration: Duration(seconds: 3),
+                  glowColor: Color(0xFF9195FF),
+                  repeat: true,
+                  repeatPauseDuration: Duration(seconds: 2),
+                  startDelay: Duration(seconds: 1),
+                  child: Material(
+                      elevation: 8.0,
+                      shape: CircleBorder(),
+                      child: CircleAvatar(
+                        backgroundColor: Colors.grey[100],
+                        child: Image.asset(
+                          'assets/images/capiie.png',
+                          width: 45,
+                        ),
+                        radius: 30.0,
+                      )),
+                ),
+              ),
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+}
