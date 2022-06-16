3284ef900a9ea34cbf104294fe4d167fd1c02f43
fepinheir0 <f.pinheir0@outlook.com>
2022-06-16 00:14:22 -0300

    adicionando tora ao main e retirando pagina de rotas

--- capiie/lib/main.dart
+++ capiie/lib/main.dart
@@ -1,6 +1,21 @@
-import 'package:capiie/routes/app_widget.dart';
+import 'package:capiie/modules/login/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() async {
-  runApp(AppWidget());
+  runApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Capiie Totem',
+      theme: ThemeData.dark(),
+      home: LoginPage(),
+    );
+  }
 }
