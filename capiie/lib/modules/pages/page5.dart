a3b8ccdfee51c38e615fc4f2d431bb7472680b8d
fepinheir0 <f.pinheir0@outlook.com>
2022-06-16 01:50:14 -0300

    corrigindo import

--- capiie/lib/modules/pages/page5.dart
+++ capiie/lib/modules/pages/page5.dart
@@ -1,6 +1,5 @@
 import 'package:avatar_glow/avatar_glow.dart';
 import 'package:capiie/modules/pages/page_foto.dart';
-import 'package:capiie/modules/pages/page_resumo.dart';
 import 'package:capiie/utilidades/delayed_animation.dart';
 import 'package:flutter/material.dart';
 
