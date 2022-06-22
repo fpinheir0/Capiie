143cb24bbeb67d9216a53212a89dd5ade8e0aa43
fepinheir0 <f.pinheir0@outlook.com>
2022-06-22 18:19:47 -0300

    removendo arquivos não utilizados

--- capiie/lib/modules/pages/bloc/resumo_bloc.dart
+++
@@ -1,7 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:capiie/modules/pages/bloc/resumo_event.dart';
-import 'package:capiie/modules/pages/bloc/resumo_state.dart';
-
-class resumoBloc extends Bloc<resumoEvent, resumoPageState> {
-  resumoBloc(super.initialState);
-}
