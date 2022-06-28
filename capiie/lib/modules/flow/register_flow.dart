607eb79582d382fe60a0f711788c0d836f12bb0d
fepinheir0 <f.pinheir0@outlook.com>
2022-06-27 23:58:16 -0300

    criando o registro flow

---
+++ capiie/lib/modules/flow/register_flow.dart
@@ -0,0 +1,62 @@
+import 'package:capiie/modules/pages/bloc/register_bloc.dart';
+import 'package:capiie/modules/pages/bloc/registro_state.dart';
+import 'package:capiie/modules/pages/register_cargo_page.dart';
+import 'package:capiie/modules/pages/register_email_page.dart';
+import 'package:capiie/modules/pages/register_name_page.dart';
+import 'package:capiie/modules/pages/register_telefone_page.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class RegisterFlow extends StatelessWidget {
+  RegisterFlow({Key? key}) : super(key: key);
+
+  static const String route = "/register";
+  @override
+  Widget build(BuildContext context) {
+    var bloc = BlocProvider.of<RegisterBloc>(context);
+    return BlocListener<RegisterBloc, RegisterState>(
+      listener: (context, state) {
+        if (state is RegisterCompletedState) {
+          Navigator.pop(context, bloc.register);
+        }
+      },
+      child: BlocBuilder<RegisterBloc, RegisterState>(
+        bloc: bloc,
+        builder: (context, state) {
+          return Navigator(
+            pages: pages(bloc, state),
+            onPopPage: (route, result) {
+              return route.didPop(result);
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  List<Page<dynamic>> pages(RegisterBloc bloc, RegisterState state) {
+    final list = <Page<dynamic>>[];
+    final namePage = RegisterNamePage.page(nome: bloc.register.nome);
+    final cargoPage = RegisterCargoPage.page(cargo: bloc.register.cargo);
+    final emailPage = RegisterEmailPage.page(email: bloc.register.email);
+    final telefonePage =
+        RegisterTelefonePage.page(telefone: bloc.register.telefone.toString());
+
+    if (state is RegisterNamePageState) {
+      list.add(namePage);
+    }
+
+    if (state is RegisterCargoPageState) {
+      list.addAll([namePage, cargoPage]);
+    }
+
+    if (state is RegisterEmailPageState) {
+      list.addAll([namePage, cargoPage, emailPage]);
+    }
+
+    if (state is RegisterTelefonePageState) {
+      list.addAll([namePage, cargoPage, emailPage, telefonePage]);
+    }
+    return list;
+  }
+}
