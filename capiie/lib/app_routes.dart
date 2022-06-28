984e661f9dcbce11e965d012be94400bd54bd173
fepinheir0 <f.pinheir0@outlook.com>
2022-06-27 23:58:35 -0300

    adicionando Rotas ao app

---
+++ capiie/lib/app_routes.dart
@@ -0,0 +1,28 @@
+import 'package:capiie/modules/flow/register_flow.dart';
+import 'package:capiie/modules/login/login_page.dart';
+import 'package:capiie/modules/pages/bloc/register_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AppRoutes {
+  static Route<dynamic> routes(RouteSettings settings) {
+    var newRoute;
+    switch (settings.name) {
+      case LoginPage.route:
+        newRoute = MaterialPageRoute(
+          builder: (context) => LoginPage(),
+        );
+
+        break;
+      case RegisterFlow.route:
+        newRoute = MaterialPageRoute(
+          builder: (context) => BlocProvider<RegisterBloc>(
+            create: (context) => RegisterBloc(),
+            child: RegisterFlow(),
+          ),
+        );
+        break;
+    }
+    return newRoute;
+  }
+}
